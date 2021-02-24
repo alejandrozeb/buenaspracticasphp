11c34381bedce92cdd441eb0f6aa29ae351087e3
unknown <alejandrojzeballos@gmail.com>
2021-02-23 22:11:50 -0400

    {add} beneficios del codigo limpio

--- apuntes/index.php
+++ apuntes/index.php
@@ -1,4 +1,13 @@
 <?php
-/* APuntes del curso de platzi */
+/* APuntes del curso de platzi
+    Serie de tecnicas para ser mas mantenibles y robustas.
+
+    beneficios
+    -cuando retomemos un codigo desoues e un tiempo, vamos a poder continuar con el desarrollo
+    - Beneficia al equipo de trabajo, un codigo entendible hace mas facil que otra persona pueda continuar con el trabajo
+    - el cliente tambien sale beneficiado ya que nuestro codigo sera mantenible y entendible.
+
+    Todos los involucrados salen beneficiados.
+*/
 
 print("Hi first commit");
