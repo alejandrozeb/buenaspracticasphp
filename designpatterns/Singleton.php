b5e03727585fbbaa7705668f486641c3dc5f6962
unknown <alejandrojzeballos@gmail.com>
2021-02-27 00:21:04 -0400

    {add} Comand

--- designpatterns/Singleton.php
+++ designpatterns/Singleton.php
@@ -12,4 +12,16 @@ Son ayudas de como puede funcionar nuestra aplicacion. paternitis es poner los p
     Que solo se instancie una vez y se pueda usar en diferentes partes (nodejs).
     El singleton tiene su constructor privado pero como se instancia si pasa esto.
     se crea un metodo publico que pueda instanciar el objeto y puedea verficar si ya se instanciop previamente-
+
 */
+
+/* Factory
+    es un patron creacional que ayuda en las instancias de objetos, se usa cuando la creacion de objeto es complejo muchos parametrso o calculos complejos
+    logramos codigo abstracto y reutilizable, podemos reducir la complejidad del objeto, y va sobre un nivel de abstraccion.
+*/
+/* comannd 
+    Patron de comportamiento se usa cuando eiste una operacion compleja, cuando necesitamos responder desde navegardor y otrp  de comandos en este caso la funcionalidad puede ser un objeto.
+    se implemento con una iinterface con una function execute
+
+    podemos recorrer un array de comando y el comand hara el execute solo unifica la interface
+
