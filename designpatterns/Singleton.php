95a9659a8c424766c46d5c851063bdb6c565f1e1
unknown <alejandrojzeballos@gmail.com>
2021-02-26 22:27:40 -0400

    {add} Patrones de diseño singleton

---
+++ designpatterns/Singleton.php
@@ -0,0 +1,15 @@
+<?php
+/* Patrones de diseño son soluciones conceptuales que se pueden aplicar, 
+pateones de creacion
+pateones de estructura
+pateones de comportamiento
+que afectan a las clases
+
+Son ayudas de como puede funcionar nuestra aplicacion. paternitis es poner los patrones en todas las soluciones
+*/
+
+/* Singleton
+    Que solo se instancie una vez y se pueda usar en diferentes partes (nodejs).
+    El singleton tiene su constructor privado pero como se instancia si pasa esto.
+    se crea un metodo publico que pueda instanciar el objeto y puedea verficar si ya se instanciop previamente-
+*/
